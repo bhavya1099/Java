a1603dd3fdeaf956e462a0ec1075360b9f435b1d
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-07-05 15:55:49 +0530

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/com/thealgorithms/dynamicprogramming/Categories.java
@@ -0,0 +1,225 @@
+package com.thealgorithms.dynamicprogramming;
+
+public interface Categories {
+
+	interface startAlgo {
+
+	}
+
+	interface endAlgo {
+
+	}
+
+	interface bpR {
+
+	}
+
+	interface bpRS {
+
+	}
+
+	interface bpIS {
+
+	}
+
+	interface getPixel {
+
+	}
+
+	interface putPixel {
+
+	}
+
+	interface boundaryFill {
+
+	}
+
+	interface printImageArray {
+
+	}
+
+	interface numberOfWays {
+
+	}
+
+	interface change {
+
+	}
+
+	interface minimumCoins {
+
+	}
+
+	interface printAmount {
+
+	}
+
+	interface countFriendsPairing {
+
+	}
+
+	interface DP {
+
+	}
+
+	interface findWays {
+
+	}
+
+	interface minDistance {
+
+	}
+
+	interface editDistance {
+
+	}
+
+	interface minTrials {
+
+	}
+
+	interface fibMemo {
+
+	}
+
+	interface fibBotUp {
+
+	}
+
+	interface fibOptimized {
+
+	}
+
+	interface fibBinet {
+
+	}
+
+	interface maxSum {
+
+	}
+
+	interface knapSack {
+
+	}
+
+	interface naiveLevenshteinDistance {
+
+	}
+
+	interface optimizedLevenshteinDistance {
+
+	}
+
+	interface LongestCommonSubsequence {
+
+	}
+
+	interface getLCS {
+
+	}
+
+	interface lcsString {
+
+	}
+
+	interface lis {
+
+	}
+
+	interface findLISLen {
+
+	}
+
+	interface lps {
+
+	}
+
+	interface getLongestValidParentheses {
+
+	}
+
+	interface minimumPathSum {
+
+	}
+
+	interface minimumSumPartition {
+
+	}
+
+	interface nthManShanksPrime {
+
+	}
+
+	interface execute {
+
+	}
+
+	interface getCost {
+
+	}
+
+	interface minimalpartitions {
+
+	}
+
+	interface partition {
+
+	}
+
+	interface cutRod {
+
+	}
+
+	interface ShortestSuperSequence {
+
+	}
+
+	interface shortestSuperSequence {
+
+	}
+
+	interface lcs {
+
+	}
+
+	interface getCount {
+
+	}
+
+	interface getCountSO {
+
+	}
+
+	interface subsetSum {
+
+	}
+
+	interface compute {
+
+	}
+
+	interface uniquePaths {
+
+	}
+
+	interface uniquePaths2 {
+
+	}
+
+	interface isMatch {
+
+	}
+
+	interface wpRecursion {
+
+	}
+
+	interface wptd {
+
+	}
+
+	interface wpbu {
+
+	}
+
+}
