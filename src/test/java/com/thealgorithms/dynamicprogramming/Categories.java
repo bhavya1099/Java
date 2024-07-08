cc8f30b012367949a4113617a82a036e10217f05
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-07-08 11:05:48 +0530

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/com/thealgorithms/dynamicprogramming/Categories.java
@@ -0,0 +1,445 @@
+package com.thealgorithms.dynamicprogramming;
+
+public interface Categories {
+
+	interface BoardPath {
+
+	}
+
+	interface startAlgo {
+
+	}
+
+	interface endAlgo {
+
+	}
+
+	interface bpR {
+
+	}
+
+	interface bpRS {
+
+	}
+
+	interface bpIS {
+
+	}
+
+	interface BoundaryFill {
+
+	}
+
+	interface getPixel {
+
+	}
+
+	interface putPixel {
+
+	}
+
+	interface boundaryFill {
+
+	}
+
+	interface printImageArray {
+
+	}
+
+	interface BruteForceKnapsack {
+
+	}
+
+	interface knapSack {
+
+	}
+
+	interface CatalanNumber {
+
+	}
+
+	interface findNthCatalan {
+
+	}
+
+	interface ClimbingStairs {
+
+	}
+
+	interface numberOfWays {
+
+	}
+
+	interface CoinChange {
+
+	}
+
+	interface change {
+
+	}
+
+	interface minimumCoins {
+
+	}
+
+	interface printAmount {
+
+	}
+
+	interface CountFriendsPairing {
+
+	}
+
+	interface countFriendsPairing {
+
+	}
+
+	interface DP {
+
+	}
+
+	interface findWays {
+
+	}
+
+	interface EditDistance {
+
+	}
+
+	interface minDistance {
+
+	}
+
+	interface editDistance {
+
+	}
+
+	interface EggDropping {
+
+	}
+
+	interface minTrials {
+
+	}
+
+	interface Fibonacci {
+
+	}
+
+	interface fibMemo {
+
+	}
+
+	interface fibBotUp {
+
+	}
+
+	interface fibOptimized {
+
+	}
+
+	interface fibBinet {
+
+	}
+
+	interface FordFulkerson {
+
+	}
+
+	interface networkFlow {
+
+	}
+
+	interface KadaneAlgorithm {
+
+	}
+
+	interface maxSum {
+
+	}
+
+	interface Knapsack {
+
+	}
+
+	interface throwIfInvalidInput {
+
+	}
+
+	interface LevenshteinDistance {
+
+	}
+
+	interface naiveLevenshteinDistance {
+
+	}
+
+	interface optimizedLevenshteinDistance {
+
+	}
+
+	interface LongestAlternatingSubsequence {
+
+	}
+
+	interface alternatingLength {
+
+	}
+
+	interface LongestCommonSubsequence {
+
+	}
+
+	interface getLCS {
+
+	}
+
+	interface lcsString {
+
+	}
+
+	interface LongestIncreasingSubsequence {
+
+	}
+
+	interface upperBound {
+
+	}
+
+	interface lis {
+
+	}
+
+	interface findLISLen {
+
+	}
+
+	interface binarySearchBetween {
+
+	}
+
+	interface LongestPalindromicSubsequence {
+
+	}
+
+	interface lps {
+
+	}
+
+	interface recursiveLPS {
+
+	}
+
+	interface LongestPalindromicSubstring {
+
+	}
+
+	interface LongestValidParentheses {
+
+	}
+
+	interface getLongestValidParentheses {
+
+	}
+
+	interface MatrixChainMultiplication {
+
+	}
+
+	interface printOptimalParens {
+
+	}
+
+	interface printArray {
+
+	}
+
+	interface matrixChainOrder {
+
+	}
+
+	interface input {
+
+	}
+
+	interface MatrixChainRecursiveTopDownMemoisation {
+
+	}
+
+	interface memoizedMatrixChain {
+
+	}
+
+	interface lookupChain {
+
+	}
+
+	interface MinimumPathSum {
+
+	}
+
+	interface minimumPathSum {
+
+	}
+
+	interface MinimumSumPartition {
+
+	}
+
+	interface minimumSumPartition {
+
+	}
+
+	interface NewManShanksPrime {
+
+	}
+
+	interface nthManShanksPrime {
+
+	}
+
+	interface OptimalJobScheduling {
+
+	}
+
+	interface execute {
+
+	}
+
+	interface calculateCost {
+
+	}
+
+	interface runningCost {
+
+	}
+
+	interface findMin {
+
+	}
+
+	interface showResults {
+
+	}
+
+	interface getCost {
+
+	}
+
+	interface PalindromicPartitioning {
+
+	}
+
+	interface minimalpartitions {
+
+	}
+
+	interface PartitionProblem {
+
+	}
+
+	interface partition {
+
+	}
+
+	interface RegexMatching {
+
+	}
+
+	interface regexRecursion {
+
+	}
+
+	interface regexBU {
+
+	}
+
+	interface RodCutting {
+
+	}
+
+	interface cutRod {
+
+	}
+
+	interface ShortestSuperSequence {
+
+	}
+
+	interface shortestSuperSequence {
+
+	}
+
+	interface lcs {
+
+	}
+
+	interface SubsetCount {
+
+	}
+
+	interface getCount {
+
+	}
+
+	interface getCountSO {
+
+	}
+
+	interface SubsetSum {
+
+	}
+
+	interface subsetSum {
+
+	}
+
+	interface SumOfSubset {
+
+	}
+
+	interface Tribonacci {
+
+	}
+
+	interface compute {
+
+	}
+
+	interface UniquePaths {
+
+	}
+
+	interface uniquePaths {
+
+	}
+
+	interface uniquePaths2 {
+
+	}
+
+	interface WildcardMatching {
+
+	}
+
+	interface isMatch {
+
+	}
+
+	interface WineProblem {
+
+	}
+
+	interface wpRecursion {
+
+	}
+
+	interface wptd {
+
+	}
+
+	interface wpbu {
+
+	}
+
+}
