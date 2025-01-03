c85ba17b9d2b31daa89ff631702a4262eb5bf461
bhavya1099 <bhavya.goswami99@gmail.com>
2025-01-03 13:29:45 +0530

    Improved Unit test generated by RoostGPT
    Using AI Model gpt-4, for user feedback: please remove compilation errors and add appropriate imports for the nested classes

--- src/test/java/com/thealgorithms/greedyalgorithms/JobSequencingFindJobSequenceTest.java
+++ src/test/java/com/thealgorithms/greedyalgorithms/JobSequencingFindJobSequenceTest.java
@@ -78,7 +78,7 @@ roost_feedback [1/3/2025, 10:57:46 AM]:please remove compilation errors and add 
 
 roost_feedback [1/3/2025, 11:02:48 AM]:please remove compilation errors and add appropriate imports for the nested classes
 
-roost_feedback [1/3/2025, 1:26:16 PM]:please remove compilation errors and add appropriate imports for the nested classes
+roost_feedback [1/3/2025, 1:28:06 PM]:please remove compilation errors and add appropriate imports for the nested classes
 */
 
 // ********RoostGPT********
