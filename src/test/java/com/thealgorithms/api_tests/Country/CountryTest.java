1c1db03a46647dcc7eb7e40d64fcb52a2d33bd34
bhavya1099 <kinshu.1019@gmail.com>
2025-07-16 07:50:07 +0000

    Add API Tests (Karate Framework, Azure Open AI) generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/com/thealgorithms/api_tests/Country/CountryTest.java
@@ -0,0 +1,26 @@
+
+package com.thealgorithms.api_tests.Country;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+// import com.intuit.karate.http.HttpServer;
+// import com.intuit.karate.http.ServerConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CountryTest {
+
+	@Test
+	void testAll() {
+		String URLBASE = System.getenv().get("URL_BASE");
+		String AUTHTOKEN = System.getenv().get("AUTH_TOKEN");
+		Results results = Runner.path("src/test/java/com/thealgorithms/api_tests/Country")
+			.systemProperty("URL_BASE", URLBASE)
+			.systemProperty("AUTH_TOKEN", AUTHTOKEN)
+			.reportDir("testReport")
+			.parallel(1);
+		assertEquals(0, results.getFailCount(), results.getErrorMessages());
+	}
+
+}
